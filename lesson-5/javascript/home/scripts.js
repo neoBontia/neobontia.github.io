b8f921f2217565349edaa33eb913aa1bf4c64834
neoBontia <bon21004@byui.edu>
2021-12-02 14:53:34 +0800

    added json and images

--- lesson-5/javascript/home/scripts.js
+++ lesson-5/javascript/home/scripts.js
@@ -13,4 +13,52 @@ function toggleMenu() {
 if (date.getDay() == 5) { 
     document.getElementById("banner").style.display = "block";
 }
-    
+
+//
+
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        console.table(jsonObject);  // temporary checking for valid response and data parsing
+        const towns = jsonObject['towns'];
+
+        for (let i = 0; i < towns.length; i++) {
+            let card = document.createElement('article');
+
+            let image = document.createElement('img');
+            image.setAttribute('src', "images/home/" + towns[i].photo);
+            image.setAttribute('alt', "Picture of " + towns[i].name);
+            card.appendChild(image)
+
+
+            let data = document.createElement('div');
+
+            let h2 = document.createElement('h2');
+            h2.textContent = towns[i].name;
+            data.appendChild(h2);
+
+            let motto = document.createElement('p');
+            motto.textContent = towns[i].motto;
+            data.appendChild(motto);
+
+            let year = document.createElement('p');
+            year.textContent = "Year Founded: " + towns[i].yearFounded;
+            data.appendChild(year);
+
+            let pop = document.createElement('p');
+            pop.textContent = "Current Population: " + towns[i].currentPopulation;
+            data.appendChild(pop);
+
+            let rain = document.createElement('p');
+            rain.textContent = "Average Rainfall: " + towns[i].averageRainfall;
+            data.appendChild(rain);
+            
+            card.appendChild(data)
+
+            document.querySelector('div.cards').appendChild(card);
+        }
+    });
