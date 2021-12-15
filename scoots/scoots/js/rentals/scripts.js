8962e4b9d5521a7de1ac5f0191471609ecdeb09c
neoBontia <bon21004@byui.edu>
2021-12-16 07:40:19 +0800

    Finished rentals small view

--- scoots/scoots/js/rentals/scripts.js
+++ scoots/scoots/js/rentals/scripts.js
@@ -21,19 +21,19 @@ fetch(rentalsFile)
             tr.appendChild(max);
 
             let rh = document.createElement("td");
-            rh.innerText = rental.rh;
+            rh.innerText = "$" + rental.rh;
             tr.appendChild(rh);
 
             let rf = document.createElement("td");
-            rf.innerText = rental.rf;
+            rf.innerText = "$" + rental.rf;
             tr.appendChild(rf);
 
             let wh = document.createElement("td");
-            wh.innerText = rental.wh;
+            wh.innerText = "$" + rental.wh;
             tr.appendChild(wh);
 
             let wf = document.createElement("td");
-            wf.innerText = rental.wf;
+            wf.innerText = "$" + rental.wf;
             tr.appendChild(wf);
 
             document.getElementById("rentals-table").appendChild(tr);
