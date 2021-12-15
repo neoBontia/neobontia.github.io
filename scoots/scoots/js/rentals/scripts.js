f310ec6fee0670b6a736a9d792226fa6062e002a
neoBontia <bon21004@byui.edu>
2021-12-16 05:00:39 +0800

    Retrieved data from json file.

--- scoots/scoots/js/rentals/scripts.js
+++ scoots/scoots/js/rentals/scripts.js
@@ -1,7 +1,42 @@
 let rentalsFile = "json/rentals.json";
+let rentalData = []
+
 
 fetch(rentalsFile)
     .then((response) => response.json())
     .then((jsObject) => {
-        console.log(jsObject);
+        rentalData = jsObject["rentals"];
+        console.log(rentalData);
+
+        for (let i = 0; i < rentalData.length; i++) {
+            let tr = document.createElement("tr");
+            rental = rentalData[i];
+
+            let type = document.createElement("td");
+            type.innerText = rental.type;
+            tr.appendChild(type);
+
+            let max = document.createElement("td");
+            max.innerText = rental.max;
+            tr.appendChild(max);
+
+            let rh = document.createElement("td");
+            rh.innerText = rental.rh;
+            tr.appendChild(rh);
+
+            let rf = document.createElement("td");
+            rf.innerText = rental.rf;
+            tr.appendChild(rf);
+
+            let wh = document.createElement("td");
+            wh.innerText = rental.wh;
+            tr.appendChild(wh);
+
+            let wf = document.createElement("td");
+            wf.innerText = rental.wf;
+            tr.appendChild(wf);
+
+            document.getElementById("rentals-table").appendChild(tr);
+        
+        }
     });
